3dff4905220c20d3c9ac886c89ead9fdab3ba2e6
Joe Mansour <joemansour2003@gmail.com>
2023-07-23 22:38:49 -0400

    not found

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
     return (
         <div className="container text-center">
-            <h1>404 - Page Not Found</h1>
-            <p>The page you are looking for does not exist.</p>
-            <Link to="/">Go back to the home page</Link>
+            <div className="not-found">
+                <h1>404 - Page Not Found</h1>
+                <p>The page you are looking for does not exist.</p>
+                <Link to="/">Go back to the home page</Link>
+            </div>
         </div>
     );
 };
